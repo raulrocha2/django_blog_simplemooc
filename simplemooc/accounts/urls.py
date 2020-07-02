92b19981cdfc21de18510c863c93cea0489907ca
raulrocha2 <raul.rocha2@gmail.com>
2020-07-01 23:23:31 -0300

    Foi Criado template/resgistration/register.html, e campo e-mail em cadastro de usuario

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,13 +1,14 @@
 #from django.contrib.auth import login
 from django.conf.urls import url, include
 from django.contrib.auth.views import LoginView
-
-app_name = 'Accounts'
+from simplemooc.accounts import views
+app_name = 'accounts'
 
 urlpatterns = [
 	#url(r'^entrar/', include('django.contrib.auth.urls'),
 	 #{'template_name': 'accounts/login.html'}, name='login'),
 	url(r'^entrar/', LoginView.as_view(), 
 	{'template_name': 'accounts/login.html'}, name='login'),
+	url(r'cadastrar_se', views.register, name='register')
 
 	]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from .forms import RegisterForm
 
+def register(request):
+    template_name = 'registration/register.html'
+    if request.method ==  'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+
+    else:
+        form = RegisterForm()        
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
 
 
