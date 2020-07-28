69d93e900da0686e9b38677fa22a95d1d42bbdb8
raulrocha2 <raul.rocha2@gmail.com>
2020-07-28 16:22:44 -0300

    Teste Feioto Maquina do trampo 02

--- simplemooc/courses/test/test_models.py
+++ simplemooc/courses/test/test_models.py
@@ -15,9 +15,15 @@ class CourseManageTestCase(TestCase):
 			)
 		
 
+		self.courses_django = mommy.make(
+			'courses.Course', name='Teste django', _quantity=10
+			)	
+
 	def tearDown(self):
 		Course.objects.all().delete()
 
 	def test_course_search(self):
 		search = Course.objects.search('mommy')
 		self.assertEqual(len(search), 5)		
+		search = Course.objects.search('django')
+		self.assertEqual(len(search), 10)
