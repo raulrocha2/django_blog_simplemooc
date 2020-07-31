2e7949685c1d6b0d4781779025644f4a1cc1aa00
raulrocha2 <raul.rocha2@gmail.com>
2020-07-31 06:43:35 -0300

    base dir static

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
-base dir padrao local
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
