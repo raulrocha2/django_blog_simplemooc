f083a77a8e567f23f62b773dc808b170fa10e884
raulrocha2 <raul.rocha2@gmail.com>
2020-08-02 20:07:38 -0300

    update settings

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -176,3 +176,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+try:
+    from simplemooc.local_settings import *
+except ImportError:
+    pass    
