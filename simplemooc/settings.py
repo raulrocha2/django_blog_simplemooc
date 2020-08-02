9d01ad002633155790ec1a25572e161a71f899c3
raulrocha2 <raul.rocha2@gmail.com>
2020-08-02 20:34:57 -0300

    ajustando arquivos static  settings

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -159,7 +159,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 STATIC_URL = '/static/'
 
 try:
