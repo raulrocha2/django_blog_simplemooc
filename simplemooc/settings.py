4bb38f78c9db856357c80e02713150c3ef9d85e1
raulrocha2 <raul.rocha2@gmail.com>
2020-08-02 21:20:57 -0300

    copiando pasta static para fora do core

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -158,10 +158,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-DISABLE_COLLECTSTATIC=1
+
 
 try:
     from simplemooc.local_settings import *
