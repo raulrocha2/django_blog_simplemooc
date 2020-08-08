cbe5dc687bbc92c39eebe8adfb075dab085bf70d
raulrocha2 <raul.rocha2@gmail.com>
2020-08-07 22:14:32 -0300

    configurando settings.py com static_root

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -159,20 +159,15 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
-MEDIA_URL = '/media/'
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-"""STATICFILES_DIRS = [
+STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'simplemooc/core', 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media')
-MEDIA_URL = '/media/'  """
+MEDIA_URL = '/media/' 
 
  
 try:
