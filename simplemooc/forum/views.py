a4183d6cf11928a682335bcc793503d945e51ad6
raulrocha2 <raul.rocha2@gmail.com>
2020-08-17 20:41:40 -0300

    modifed requirements.txt taggit

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -7,9 +7,19 @@ from .models import Thread
 
 
 class ForumView(ListView):
-    model = Thread
+    
     paginate_by = 2
     template_name = 'forum/index.html'
+
+
+    def get_queryset(self):
+        queryset = Thread.objects.all()
+        order = self.request.GET.get('order', '')
+        if order == 'views':
+            queryset = queryset.order_by('-views')
+        elif order == 'answers':
+            queryset = queryset.order_by('-answers')
+        return queryset        
 
     def get_context_data(self, **kwargs):
         context = super(ForumView, self).get_context_data(**kwargs)
